e0b5cd14cfc0e4aa582099944b23e69b3cfa7f15
HiwotAddis <addishiwot963@gmail.com>
2023-09-09 00:59:53 -0700

    . alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	/* your code goes there */
+	return (0);
+}

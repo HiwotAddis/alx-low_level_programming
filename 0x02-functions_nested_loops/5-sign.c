6f7d377b7f1ab8ea52296517d9660b205fc15e01
HiwotAddis <addishiwot963@gmail.com>
2023-09-13 14:12:42 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - it prints sign
+ * Return: 0
+ * @n: parameter
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
